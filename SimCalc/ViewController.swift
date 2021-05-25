6d570c0cba46bc5736650ffa67389165e16cd651
wiwi-git <duse34@hotmail.com>
2021-05-25 20:44:18 +0900

    Initial Commit

---
+++ SimCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimCalc
+//
+//  Created by 위대연 on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
