b8833007e9eebc703d5a79bed816bdc64b01ec6b
wiwi-git <duse34@hotmail.com>
2021-06-30 10:50:43 +0900

    소스트리로 커밋테스트으으

--- SimCalc/AppDelegate.swift
+++ SimCalc/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
+// 테스트 수정
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
